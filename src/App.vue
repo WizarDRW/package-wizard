e9f74ca4792f94f9564163cfea689d09fc904d93
Burak <wizard1456@hotmail.com>
2021-10-13 21:51:00 +0300

    Package frontend started.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>Packages of Wizard's Orbit</h1>
+  <ul>
+    <li>
+      <h2>
+        <a href="/wo-terminal" target="_self" rel="">WO Terminal(Vue3)</a> --
+        <span><i>a Vue 3 terminal project</i></span>
+      </h2>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+</script>
+
+<style>
+#app {
+  font-family: monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul,
+li,
+a {
+  text-decoration: none;
+  list-style: none;
+  text-align: left;
+  padding-left: 150px;
+}
+</style>
